61e7b05de1a2933db8d3c13c72447816cdd4e038
Adam V <parduckids@gmail.com>
2024-04-13 18:16:42 +0000

    feat(auth): add user registration and login using modals with bcrypt hashing and AJAX interactions

--- waveandleaf/routes.py
+++ waveandleaf/routes.py
@@ -1,8 +1,58 @@
-from flask import render_template
+from flask import render_template, request, jsonify
 from waveandleaf import app, db
 from waveandleaf.models import User, Category, Recipe, DifficultyLevel
+# import bcrypt flask extension that provides bcrypt hashing utilities
+from flask_bcrypt import Bcrypt
 
-
+# home page
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+
+# _________________________ authentication ______________________________
+
+bcrypt = Bcrypt()
+
+# register
+# create route for /register which will be triggered by the register button using js
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.form['username']
+    password = request.form['password']
+    user = User.query.filter_by(username=username).first()
+    if user:
+        return jsonify({'error': 'Username already exists'}), 409
+    ''' 
+    this bcrypt function takes a plain text password as input and uses the bcrypt
+    hashing algorithm to create a hash of the password. Hashing is a security measure
+    to ensure that plain text passwords are not stored in the database, protecting them
+    from being exposed even if the database is compromised.
+    '''
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+    new_user = User(username=username, password_hash=hashed_password)
+    # add user to db 
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'Registered successfully. Please login.'}), 201
+
+# login
+# create route for /login which will be triggered by the login button using js
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    user = User.query.filter_by(username=username).first()
+    # if the user exist, present the message
+    if user and bcrypt.check_password_hash(user.password_hash, password):
+        
+        return jsonify({'message': 'Logged in successfully!'}), 200
+    # if the user doesn't exist, show error
+    return jsonify({'error': 'Invalid username or password'}), 401
+
+
+
+
+# _________________________ authentication ends ______________________________
--- waveandleaf/static/js/scripts.js
+++ waveandleaf/static/js/scripts.js
@@ -3,3 +3,66 @@ const currentYear = new Date().getFullYear();
 
 // user id "footer-year" and set its text content to the current year
 document.getElementById("footer-year").textContent = "© " + currentYear + " Wave & Leaf";
+
+
+
+// modal auth
+
+// once the page is loaded successfully
+$(document).ready(function() {
+    // use ajax post request to the /register route that's created with flask
+    $('#registrationModal .btn-success').click(function() {
+        var username = $('#registrationModal #username').val();
+        var password = $('#registrationModal #password').val();
+        $.ajax({
+            type: 'POST',
+            url: '/register',
+            data: {
+                username: username,
+                password: password
+            },
+            // hide modal and show alert if successful registration
+            success: function(response) {
+                alert(response.message);
+                $('#registrationModal').modal('hide');
+            },
+            error: function(xhr, status, error) {
+                // check if responseJSON is defined, use jsonified message that flask sends
+                if(xhr.responseJSON && xhr.responseJSON.error){
+                    alert(xhr.responseJSON);
+                } else {
+                    // general error message or parse the status or error thrown by jQuery
+                    alert('Failed to register: ' + error);
+                }
+            }
+        });
+    });
+    // use ajax post request to the /login route that's created with flask
+    $('#loginModal .btn-outline-success').click(function() {
+        var username = $('#loginModal #loginUsername').val();
+        var password = $('#loginModal #loginPassword').val();
+        $.ajax({
+            type: 'POST',
+            url: '/login',
+            data: {
+                username: username,
+                password: password
+            },
+            // hide modal and show alert if successful registration
+            success: function(response) {
+                alert(response.message);
+                $('#loginModal').modal('hide');
+                // optionally, redirect to another page or update UI
+            },
+            error: function(xhr, status, error) {
+                // check if responseJSON is defined, use jsonified message that flask sends
+                if(xhr.responseJSON && xhr.responseJSON.error){
+                    alert(xhr.responseJSON.error);
+                } else {
+                    // general error message or parse the status or error thrown by jQuery
+                    alert('Login failed: ' + error);
+                }
+            }
+        });
+    });
+});
