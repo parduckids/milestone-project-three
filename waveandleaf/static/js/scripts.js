21367c8b9d5ccb3941b6904d6fbff3bbcb93f67b
Adam V <parduckids@gmail.com>
2024-04-13 17:02:12 +0000

    feat: set up base.html with footer and dynamic content wrapper

--- waveandleaf/static/js/scripts.js
+++ waveandleaf/static/js/scripts.js
@@ -1,1 +1,5 @@
-alert("hello")
+// get the current year
+const currentYear = new Date().getFullYear();
+
+// user id "footer-year" and set its text content to the current year
+document.getElementById("footer-year").textContent = "© " + currentYear + " Wave & Leaf";
