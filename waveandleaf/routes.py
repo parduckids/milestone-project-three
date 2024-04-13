be4fa2ee99dde7063b69860898b1438bd6c32b8f
Adam V <parduckids@gmail.com>
2024-04-13 17:07:31 +0000

    feat: set up and test  home page using base.html template

--- waveandleaf/routes.py
+++ waveandleaf/routes.py
@@ -5,4 +5,4 @@ from waveandleaf.models import User, Category, Recipe, DifficultyLevel
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("home.html")
